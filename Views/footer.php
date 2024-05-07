bb66717b9fc49b78c733e52f276effc654112b6a
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-07 19:14:29 +0200

    init scaffoldin

---
+++ Views/footer.php
@@ -0,0 +1,7 @@
+</body>
+<footer>
+    <div class="container text-center text-dark">
+        <h5>made with &hearts; </h5>
+    </div>
+</footer>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include __DIR__ . "/Views/header.php";
+include __DIR__ . "/Models/Movie.php";
+?>
+
+<main>
+    oop
+</main>
+
+<?php
+include __DIR__ ."/views/footer.php";
